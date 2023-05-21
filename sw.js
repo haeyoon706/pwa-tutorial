f030ee8bef628ec2456342d74de676e55f834498
developer-heyy <121794924+haeyoon706@users.noreply.github.com>
2023-05-21 20:10:04 +0900

    fetch 이벤트 구현 및 동작 확인

--- sw.js
+++ sw.js
@@ -5,12 +5,27 @@ var filesToCache = ["/", "/css/app.css"];
 
 // 서비스 워커 설치 (웹 자원 캐싱)
 self.addEventListener("install", function (event) {
+  console.log("Install");
   event.waitUntil(
     caches
       .open(CACHE_NAME) // pwa 파일
       .then(function (cache) {
         // pwa 파일에 다 집어 넣어라
         return cache.addAll(filesToCache);
+      })
+      .catch(function (error) {
+        return console.log(error);
+      })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  console.log("Fetch");
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(function (response) {
+        return response || fetch(event.request);
       })
       .catch(function (error) {
         return console.log(error);
